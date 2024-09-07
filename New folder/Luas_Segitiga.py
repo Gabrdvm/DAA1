b2e39bde22089056c040b2568b28e844de1257d6
Gabrdvm <gabriel.manalu04@gmail.com>
2024-09-07 20:16:23 +0700

    Initial commit

---
+++ New folder/Luas_Segitiga.py
@@ -0,0 +1,8 @@
+def hitung_luas_segitiga(alas, tinggi):
+    
+    return (alas * tinggi) / 2
+
+alas = 25
+tinggi = 30
+luas = hitung_luas_segitiga(alas, tinggi)
+print(f"Luas segitiga adalah: {luas}")
