482104cf91618941138f7f1abac5112ea79e4e6f
Gabrdvm <gabriel.manalu04@gmail.com>
2024-09-09 13:46:56 +0700

    Hapus Folder

--- New folder/Luas_Segitiga.py
+++
@@ -1,8 +0,0 @@
-def hitung_luas_segitiga(alas, tinggi):
-    
-    return (alas * tinggi) / 2
-
-alas = 25
-tinggi = 30
-luas = hitung_luas_segitiga(alas, tinggi)
-print(f"Luas segitiga adalah: {luas}")
